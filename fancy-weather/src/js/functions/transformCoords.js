ee726e421516e6b4619133089ae1aa2c5115c799
Veronika Martinovich <veranika.mart@gmail.com>
2020-06-03 13:31:44 +0300

    fix: change coords calculation in degrees and minutes

--- fancy-weather/src/js/functions/transformCoords.js
+++ fancy-weather/src/js/functions/transformCoords.js
@@ -1,7 +1,5 @@
 export function transformCoords(coord) {
-  const coordShort = coord.toFixed(2);
-  const coordArr = coordShort.split(".");
-  coordArr[0] = `${coordArr[0]}°`;
-  coordArr[1] = `${coordArr[1]}'`;
-  return coordArr.join("");
+  const coordDegrees = parseInt(coord);
+  const coordMinutes = Math.abs(Math.round((coord - coordDegrees) * 60));
+  return `${coordDegrees}°${coordMinutes}'`
 }
